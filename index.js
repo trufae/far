1e6b1c1368c8d366e7e145d01d9a35f0935963d0
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2019-06-15 01:30:12 +0200

    Swap arguments in `far` to handle multiple paths

--- index.js
+++ index.js
@@ -8,27 +8,29 @@ function loadFile (fn) {
 }
 
 if (process.argv.length < 3) {
-  console.error('Usage: far [path|glob|file] [expr]');
+  console.error('Usage: far [expr] [path|glob|file]');
   process.exit(1);
 }
-const path = process.argv[2] === '-' ? '/dev/stdin' : process.argv[2];
-const expr = process.argv[3] || '.help()';
+const path = process.argv[3] === '-' ? ['/dev/stdin'] : process.argv.slice(3);
+const expr = process.argv[3]?process.argv[2] : '.help()';
 
 if (path === '-h') {
   loadFile().help();
   process.exit(0);
 }
 
-glob(path, (err, files) => {
-  if (err) {
-    throw err;
-  }
-  files.forEach((fileName) => {
-    try {
-      eval('loadFile(fileName)' + expr);
-    } catch (e) {
-      console.error(fileName + ': ' + e.toString());
-      process.exitValue = 1;
+for (let p of path) {
+  glob(p, (err, files) => {
+    if (err) {
+      throw err;
     }
+    files.forEach((fileName) => {
+      try {
+        eval('loadFile(fileName)' + expr);
+      } catch (e) {
+        console.error(fileName + ': ' + e.toString());
+        process.exitValue = 1;
+      }
+    });
   });
-});
+}
